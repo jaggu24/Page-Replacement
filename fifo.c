7949fbed6ab526fffc673c7ddce471fa75c57d14
jaggu24 <hanumanjeyakumaran@gmail.com>
2020-03-26 18:14:13 +0530

    Algorithms implemented using C

---
+++ fifo.c
@@ -0,0 +1,146 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct queue
+{
+    int rear,front,size;
+    int capacity;
+    int *array;
+};
+struct queue* createqueue(int capacity)
+{
+    struct queue* queue = (struct queue*) malloc(sizeof(struct queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+    queue->rear = -1;
+    queue->array = (int*)malloc(queue->capacity * sizeof(int));
+    return queue;
+}
+int isfull(struct queue *q)
+{
+    return(q->size==q->capacity);
+}
+int isempty(struct queue *q)
+{
+    return(q->size==0);
+}
+void enqueue(struct queue *q,int val)
+{
+    if(isfull(q))
+        return;
+    q->rear=q->rear+1;
+    int k=q->rear;
+    q->array[k]=val;
+    //printf("enqued element is %d\n",q->array[k]);
+    q->size=q->size+1;
+}
+int dequeue(struct queue *q)
+{
+    if(isempty(q))
+        return 0;
+    int k=q->array[q->front];
+    q->front=q->front+1;
+    q->size=q->size-1;
+    //printf("deleted element is %d\n",k);
+    return k;
+}
+int set_present(int *ar,int item,int capacity)
+{
+    int i;
+    for(i=0;i<capacity;i++)
+    {
+        if(ar[i]==item)
+            return 1;
+    }
+    return 0;
+}
+int find_index(int *arr,int item,int capacity)
+{
+    int i;
+    for(i=0;i<capacity;i++)
+        if(arr[i]==item)
+            return i;
+}
+int page_replacement(int capacity,int n,int* array)
+{
+    int place=0,pagefault=0,i;
+    struct queue *q;
+    q=createqueue(capacity);
+    int set[capacity];
+    for(i=0;i<capacity;i++)
+        set[i]=-1;
+    for(i=0;i<n;i++)
+    {
+        if(q->size<capacity)
+        {
+            if(set_present(set,array[i],capacity)==0)
+            {
+                enqueue(q,array[i]);
+                set[place]=array[i];
+                place=place+1;
+                pagefault=pagefault+1;
+            }
+        }
+        else
+        {
+            if(set_present(set,array[i],capacity)==0)
+            {
+                int dele=dequeue(q);
+                int index=find_index(set,dele,capacity);
+                set[index]=array[i];
+                enqueue(q,array[i]);
+                pagefault=pagefault+1;
+            }
+        }
+    }
+    return pagefault;
+}
+int main()
+{
+    int *str,size=0;
+    FILE *fptr;
+    char ab[50],sq[10];
+    fptr=fopen("r1.txt","r");
+    if(fptr==NULL)
+    {
+            printf("No such file ");
+            return 2;
+    }
+    int a;
+    size_t size2=20;
+    char *buf;
+    buf=(char *)malloc(size2);
+    while(!feof(fptr))                                                              //Checking no. of elements in file
+    {
+            getdelim(&buf,&size2,',',fptr);
+            if(buf[1]=='\n')
+                    break;
+            if(buf[strlen(buf)-1]==',')
+                    buf[strlen(buf)-1]='\0';
+            a=atoi(buf);
+            size+=1;
+    }
+    fclose(fptr);
+    fptr=fopen("r1.txt","r");
+    str=(int *)malloc(sizeof(int)*size+1);                                          //Allocate desired size
+    int length = 0;
+    while(!feof(fptr))                                                              //Read Storing elements into str
+    {
+            getdelim(&buf,&size2,',',fptr);
+            if(buf[1]=='\n')
+                    break;
+            if(buf[strlen(buf)-1]==',')
+                    buf[strlen(buf)-1]='\0';
+            a=atoi(buf);
+            str[length]=a;
+            length+=1;
+    }
+    fclose(fptr);
+    int out;
+    for(int i=4;i<=7;i++)
+    {
+        out=page_replacement(i,size,str);
+        printf("# of pagefault is %d\n",out);
+    }
+    return 0;
+}
---
+++ lru.c
@@ -0,0 +1,148 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+struct mainmemory
+{
+    int* frame;
+    int size;
+};
+struct mainmemory* create(int si)
+{
+    struct mainmemory* s=(struct mainmemory *)malloc(sizeof(struct mainmemory));
+    s->size=0;
+    s->frame=(int *)malloc(si * sizeof(int));
+    return s;
+};
+int element_present(int *ar,int item,int capacity)
+{
+    int i;
+    for(i=0;i<capacity;i++)
+    {
+        if(ar[i]==item)
+            return 1;
+    }
+    return 0;
+}
+int lru_check(int *array,int capacity)
+{
+    int i=0,min=99,index;
+    for(i=0;i<capacity;i++)
+    {
+        if(array[i]<min)
+        {
+            min=array[i];
+            index=i;
+        }
+    }
+    return index;
+}
+int find_index(int *arr,int item,int capacity)
+{
+    int i;
+    for(i=0;i<capacity;i++)
+        if(arr[i]==item)
+            return i;
+}
+int page_replacement(int n,int capacity,int* pages)                //MAIN LRU ALGORITHM IMPLEMENTED
+{
+    struct mainmemory *mm;
+    mm=create(capacity);                                             //CREATED A MAINMEMORY WHICH HOLDS THE FRAMES
+    int refer[capacity];                                            //STORES THE REFERENCE VALUE (WHEN THE ELEMENT IS REFERED RECENTLY)
+    int i=0,pagefault=0;
+    for(i=0;i<capacity;i++)
+        mm->frame[i]=-1;                                     //INITIALISING ALL THE VALUE OF FRAMES AS -1
+    for(i=0;i<n;i++)                                            //RUNNING THE LOOP FOR ALL PAGES REFERED(N)
+    {
+        if(mm->size<capacity)                                //IF SOME FRAMES ARE NOT ASSIGNED TO ANY PAGE
+        {
+            if(element_present(mm->frame,pages[i],capacity)==0)      //FUNCTION RETURNS 0 IF THE PAGE IS PRESENT IN MAINMEMORY
+            {
+                pagefault++;
+                int index=mm->size;                          //MAINMEMORY SIZE IS THE INDEX OF NEXT PAGE TO STORE IN FRAME ARRAY
+                mm->frame[index]=pages[i];
+                refer[index]=i;                                 //STORES THE VALUE OF I AS THE REFERENCE VALUE FOR THAT PAGE
+                mm->size++;
+                //printf("inserted value %d in index %d\n",mm->frame[index],index);
+            }
+            else                                            //IF THE ELEMENT IS PRESENT IN MAIN MEMORY
+            {
+                int index=find_index(mm->frame,pages[i],capacity);         //FIND THE INDEX OF THE PAGE IN MAINMEMORY
+                refer[index]=i;                                         //CHANGING THE REFERENCE VALUE FOR THAT PAGE AS I
+            }
+        }
+        else                                                   // IF ALL THE FRAMES ARE FULL IN MAINMEMORY
+        {
+            if(element_present(mm->frame,pages[i],capacity)==0)             //IF THE PAGE IS NOT PRESENT IN MAINMEMORY
+            {
+                int index=lru_check(refer,capacity);                        //RETURNS THE INDEX OF THE PAGE WHICH IS LEAST RECENTLY USED
+                //printf("Changed the value %d in index %d \n",pages[i],index);
+                mm->frame[index]=pages[i];                      //CHANGING THE FRAME IN THAT INDEX TO NEW PAGE
+                refer[index]=i;                                 //ALSO CHANGES THE REFERENCE VALUE TO THE NEW PAGE IN THE SAME INDEX
+                pagefault++;
+            }
+            else
+            {
+                int index=find_index(mm->frame,pages[i],capacity);       //SAME INSTRUCTION IN THE FIRST IF CONDITION
+                refer[index]=i;
+            }
+        }
+    }
+    return pagefault;
+}
+int main()
+{
+    int *str,size=0;
+    FILE *fptr;
+    char ab[50],sq[10];
+    fptr=fopen("r1.txt","r");
+    if(fptr==NULL)
+    {
+            printf("No such file ");
+            return 2;
+    }
+    int a;
+    size_t size2=20;
+    char *buf;
+    buf=(char *)malloc(size2);
+    while(!feof(fptr))                                                              //Checking no. of elements in file
+    {
+            getdelim(&buf,&size2,',',fptr);
+            if(buf[1]=='\n')
+                    break;
+            if(buf[strlen(buf)-1]==',')
+                    buf[strlen(buf)-1]='\0';
+            a=atoi(buf);
+            size+=1;
+    }
+    fclose(fptr);
+    fptr=fopen("r1.txt","r");
+    str=(int *)malloc(sizeof(int)*size+1);                                          //Allocate desired size
+    int length = 0;
+    while(!feof(fptr))                                                              //Read Storing elements into str
+    {
+            getdelim(&buf,&size2,',',fptr);
+            if(buf[1]=='\n')
+                    break;
+            if(buf[strlen(buf)-1]==',')
+                    buf[strlen(buf)-1]='\0';
+            a=atoi(buf);
+            str[length]=a;
+            length+=1;
+    }
+    fclose(fptr);
+    int out;
+    for(int i=4;i<=7;i++)
+    {
+        out=page_replacement(size,i,str);
+        printf("# of pagefault is %d\n",out);
+    }
+    return 0;
+}
+/*
+void main()
+{
+    int pages[13]={7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2};
+    int out=page_replacement(13,4,pages);            //NO OF PAGES,NO OF FRAMES,PAGES REFERED ARRAY
+    printf("# of pagefault is %d\n",out);
+}
+*/
