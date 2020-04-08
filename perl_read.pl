5ac519cc7dd97e859916c590661d05fbd91e36f5
jaggu24 <hanumanjeyakumaran@gmail.com>
2020-04-08 20:19:24 +0530

    Line Chart of Frames VS pageFaults of same algo

--- perl_read.pl
+++ perl_read.pl
@@ -1,5 +1,26 @@
 # print "hello";
 use Chart::Gnuplot;
+use Test::More;
+use Test::Deep;
+
+my $o_file = "output.txt";
+my $bin = './lru';
+my $bc = './opt';
+my $bd = './fifo';
+my $be = './sec';
+my $bf = './Add';
+my $bg = './lfu';
+my $bh = './mfu';
+# run the C program
+$result = `$bin > output.txt`;
+$result = `$bc >> output.txt`;
+#$result = `$bd >> output.txt`;
+$result = `$be >> output.txt`;
+$result = `$bf >> output.txt`;
+$result = `$bg >> output.txt`;
+$result = `$bh >> output.txt`;
+print $result;
+
 open(data, "<", "output.txt");
 #print data[1];
 my $i = 0;
@@ -18,6 +39,15 @@ foreach $line (<data>)
 	{
 		print "i=2";
 		$save1 = "sec.jpeg";
+	}elsif($i == 3)
+	{
+		$save1 = "Additional_bit.jpeg";
+	}elsif($i == 4)
+	{
+		$save1 = "lfu.jpeg";
+	}elsif($i == 5)
+	{
+		$save1 = "mfu.jpeg";
 	}else{
 		print "no";
 	}
@@ -29,22 +59,21 @@ foreach $line (<data>)
 	# Create chart object and specify the properties of the chart
 	my $chart = Chart::Gnuplot->new(
 		output => $save1,
-		title  => "Simple testing",
-		xlabel => "My x-axis label",
-		ylabel => "My y-axis label",
+		title  => $save1,
+		xlabel => "No.of frames",
+		ylabel => "Page Fault",
 	);
 	
 	# Create dataset object and specify the properties of the dataset
 	my $dataSet = Chart::Gnuplot::DataSet->new(
 		xdata => \@x,
 		ydata => \@y,
-		title => "Plotting a line from Perl arrays",
+		title => "Plotting no.frames VS Pagefault",
 		style => "linespoints",
 	);
 	
 	# Plot the data set on the chart
 	$chart->plot2d($dataSet);
-	print "\n";
 	$i += 1;
 	print $i;
 	#	print $line;
