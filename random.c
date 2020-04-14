639f5924c2b2f665f01ef6b6f999c56e521a6fb8
jaggu24 <hanumanjeyakumaran@gmail.com>
2020-04-14 20:45:54 +0530

    Random Selection Algorithm

---
+++ random.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+int checked(int *pages,int n,int element)
+{
+    int flag=0;
+    for(int i=0;i<=n;i++)
+    {
+        if(pages[i]==element)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}int pri(int *ar, int n)
+{
+    for(int i=0;i<n;i++)
+        printf("%d,",ar[i]);
+    return 0;
+}
+int page_replacement(int size,int n,int *array)
+{
+    int *pages,j=0,fault=0;
+    size_t size_page=n;
+    pages=(int *)calloc(size_page,sizeof(int));
+    for(int i=0;i<size;i++)
+    {
+        if(j<n)
+        {
+            pages[j]=array[i];
+            j+=1;
+            fault+=1;
+            continue;
+        }
+//        printf("a%d  ",array[i]);
+        int check=checked(pages,n,array[i]);
+//        printf("C%d ",check);
+        if(check==0)
+        {
+            int flag=0;
+            int num;
+            while(!flag)
+            {
+                srand(time(0));
+                num = rand();
+                // printf("a%da",num%n);
+                if(num >= 0)
+                {
+                    flag+=1;
+                    num %= n;
+                }
+            }
+        //     printf("Before ");
+        //     pri(pages,n);
+            pages[num]=array[i];
+            fault+=1;
+        }
+    }
+    free(pages);
+    return fault;
+}
+int main(int argc, char *argv[])
+{
+    int *str,size=0;
+    FILE *fptr;
+    char ab[50],sq[10];
+    fptr=fopen("r1.txt","r");
+    if(fptr==NULL)
+    {
+            printf("No such file ");
+            return 2;
+    }
+    int a;
+    size_t size2=20;
+    char *buf;
+    buf=(char *)malloc(size2);
+    while(!feof(fptr))                                                              //Checking no. of elements in file
+    {
+            getdelim(&buf,&size2,',',fptr);
+            if(buf[1]=='\n')
+                    break;
+            if(buf[strlen(buf)-1]==',')
+                    buf[strlen(buf)-1]='\0';
+            a=atoi(buf);
+            size+=1;
+    }
+    fclose(fptr);
+    fptr=fopen("r1.txt","r");
+    str=(int *)malloc(sizeof(int)*size+1);                                          //Allocate desired size
+    int length = 0;
+    while(!feof(fptr))                                                              //Read Storing elements into str
+    {
+            getdelim(&buf,&size2,',',fptr);
+            if(buf[1]=='\n')
+                    break;
+            if(buf[strlen(buf)-1]==',')
+                    buf[strlen(buf)-1]='\0';
+            a=atoi(buf);
+            str[length]=a;
+            length+=1;
+    }
+    fclose(fptr);
+    int out;
+    int end1=atoi(argv[2]),inc1=atoi(argv[3]);
+    for(int i=atoi(argv[1]);i<=end1;i+=inc1)
+    {
+        out=page_replacement(size,i,str);
+        printf("%d,",out);
+    }
+    printf("\n");
+    return 0;
+}
